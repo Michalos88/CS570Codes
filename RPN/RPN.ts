2be787350c783c95fd3059444e0b84b3e5d8dc3a
mlyskawi <mlyskawi@stevens.edu>
2017-03-18 13:03:21 -0400

    Added queue

---
+++ RPN/RPN.ts
@@ -0,0 +1,48 @@
+/**
+ * Created by mlyskawi on 3/18/2017.
+ */
+
+class Queue<T>{
+
+    private arr: T[];
+
+    constructor(arr?: T[]) {
+        this.arr = arr || [];
+    }
+
+    /**
+     * enqueue
+     */
+    public enqueue(value: T) {
+        this.arr.push(value);
+    }
+
+    /**
+     * dequeue
+     */
+    public dequeue(): T {
+        return this.arr.shift();
+    }
+
+    /**
+     * peek
+     */
+    public peek(): T {
+        return this.arr[0];
+    }
+
+    /**
+     * get length
+     */
+    public get length() {
+        return this.arr.length;
+    }
+
+    /**
+     * clean
+     */
+    public clean() {
+        this.arr = [];
+    }
+
+}
