c93bfc1d92baced1b5f547c84cb4adf29cc4e301
mlyskawi <mlyskawi@stevens.edu>
2017-03-26 23:26:30 -0400

    Perfectly working w/o comments

--- Lab_5/SortedSet.ts
+++ Lab_5/SortedSet.ts
@@ -1,6 +1,9 @@
 /**
  * Created by mlyskawi on 3/26/2017.
  */
+
+// Reference: Learning JavaScript Data Structures and Algorithms, by Loiane Groner
+
 import * as fs from 'fs';
 import * as path from 'path';
 import * as readline from 'readline';
@@ -33,14 +36,14 @@ fs.readFile(path.join(__dirname,"infile"+".dat"),"utf-8", (err, file) => {
 });
 
 
-
-
 function SortedSet() {
+
     let Node = function (key) {
         this.key = key;
         this.left = null;
         this.right = null;
     };
+
     let root = null;
 
 
@@ -82,64 +85,62 @@ function SortedSet() {
 
 
     this.contains = function(key){
-        return searchNode(root, key); //{1}
+        return searchNode(root, key);
     };
 
     let searchNode = function(node, key){
-        if (node === null){ //{2}
+        if (node === null){
             return false;
         }
-        if (key < node.key){ //{3}
-            return searchNode(node.left, key); //{4}
-        } else if (key > node.key){ //{5}
-            return searchNode(node.right, key); //{6}
-        } else {
-            return true; //{7}
+        if (key < node.key){
+            return searchNode(node.left, key);
+        } else if (key > node.key){
+            return searchNode(node.right, key);
+        }
+        else {
+            return true;
         }
     };
 
     let findMinNode = function (node) {
         if (node){
-            while (node && node.left !== null) { //{2}
-                node = node.left; //{3}
+            while (node && node.left !== null) {
+                node = node.left;
             }
             return node;
         }
-        return null; //{4}
+        return null;
     };
     this.remove = function(key){
-        root = removeNode(root, key); //{1}
+        root = removeNode(root, key);
     };
 
     let removeNode = function(node, key){
-        if (node === null){ //{2}
+        if (node === null){
             return null;
         }
-        if (key < node.key){ //{3}
-            node.left = removeNode(node.left, key); //{4}
-            return node; //{5}
-        } else if (key > node.key){ //{6}
-            node.right = removeNode(node.right, key); //{7}
-            return node; //{8}
-        } else { // key is equal to node.key
-//case 1 - a leaf node
-            if (node.left === null && node.right === null){ //{9}
-                node = null; //{10}
-                return node; //{11}
+        if (key < node.key){
+            node.left = removeNode(node.left, key);
+            return node;
+        } else if (key > node.key){
+            node.right = removeNode(node.right, key);
+            return node;
+        } else {
+            if (node.left === null && node.right === null){
+                node = null;
+                return node;
             }
-//case 2 - a node with only 1 child
-            if (node.left === null){ //{12}
-                node = node.right; //{13}
-                return node; //{14}
-            } else if (node.right === null){ //{15}
-                node = node.left; //{16}
-                return node; //{17}
+            if (node.left === null){
+                node = node.right;
+                return node;
+            } else if (node.right === null){
+                node = node.left;
+                return node;
             }
-//case 3 - a node with 2 children
-            let aux = findMinNode(node.right); //{18}
-            node.key = aux.key; //{19}
-            node.right = removeNode(node.right, aux.key); //{20}
-            return node; //{21}
+            let aux = findMinNode(node.right);
+            node.key = aux.key;
+            node.right = removeNode(node.right, aux.key);
+            return node;
         }
     };
 
